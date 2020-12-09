25c1a4f5e676125cf8db07d5dcc14cf55e5e4084
michael-williamson <michaeldavid1721@yahoo.com>
2020-12-09 17:01:42 -0600

    initial commit after project builder

---
+++ src/main/java/com/project/trade_buddy/TradeBuddyApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.trade_buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeBuddyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeBuddyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/trade_buddy/TradeBuddyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.trade_buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeBuddyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
