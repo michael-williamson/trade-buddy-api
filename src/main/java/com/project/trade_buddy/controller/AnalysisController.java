bc7b4c621a22546e971e7136810f29997b8d2763
michael-williamson <michaeldavid1721@yahoo.com>
2020-12-10 10:29:45 -0600

    initialized controllers

---
+++ src/main/java/com/project/trade_buddy/controller/AnalysisController.java
@@ -0,0 +1,43 @@
+package com.project.trade_buddy.controller;
+
+import java.security.Principal;
+import java.util.List;
+
+import com.project.trade_buddy.model.Analysis;
+import com.project.trade_buddy.model.ModalFormData;
+import com.project.trade_buddy.model.Trades;
+import com.project.trade_buddy.model.User;
+import com.project.trade_buddy.services.TradeService;
+import com.project.trade_buddy.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "108.69.29.46", maxAge = 3600)
+@RestController
+@RequestMapping(path = "api/authorize", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AnalysisController {
+
+    @Autowired
+    private TradeService tradeService; 
+
+    @Autowired
+    private UserService userService; 
+
+    
+    @CrossOrigin("108.69.29.46")
+    @PostMapping(value = "/analysis")
+    public Analysis getAnalysis(@RequestBody ModalFormData formData, Principal principal){
+
+        User user = userService.findByAuthId(principal.getName());
+        List<Trades> trades = tradeService.findByDate(user,formData); 
+
+         return tradeService.analyzeTrades(trades,formData);
+    
+    }   
+}  
---
+++ src/main/java/com/project/trade_buddy/controller/TradesController.java
@@ -0,0 +1,75 @@
+package com.project.trade_buddy.controller;
+
+import java.security.Principal;
+import java.util.List;
+
+import com.project.trade_buddy.model.Trades;
+import com.project.trade_buddy.model.User;
+import com.project.trade_buddy.services.TradeService;
+import com.project.trade_buddy.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "108.69.29.46", maxAge = 3600)
+@RestController
+@RequestMapping(path = "api/authorize", produces = MediaType.APPLICATION_JSON_VALUE)
+public class TradesController {
+    @Autowired
+    private TradeService tradeService; 
+
+    @Autowired
+    private UserService userService; 
+
+    @CrossOrigin("108.69.29.46")
+    @PostMapping(value = "/trades")
+    public List<Trades> createTrade(@RequestBody Trades trades,Principal principal){
+        final User userInstance = userService.findByAuthId(principal.getName());
+
+            userInstance.addTrades(trades);   
+         userService.save(userInstance);
+            return userInstance.getTrades(); 
+
+}
+
+    @CrossOrigin("108.69.29.46")
+    @PutMapping(value = "/trades")
+    public List<Trades> updateTrade(@RequestBody Trades trade,Principal principal){
+        tradeService.save(trade);
+        final User userInstance = userService.findByAuthId(principal.getName());
+            return userInstance.getTrades(); 
+}
+
+    @CrossOrigin("108.69.29.46")
+    @DeleteMapping(value = "/trades/{id}")
+    public List<Trades> deleteTrade(@PathVariable Long id,Principal principal){
+        tradeService.deleteById(id);
+        final User userInstance = userService.findByAuthId(principal.getName());
+            return userInstance.getTrades(); 
+}
+
+
+    @CrossOrigin("108.69.29.46")
+    @GetMapping(value = "/trades")
+    public List<Trades> getUserTrades(Principal principal){
+        User user = userService.findByAuthId(principal.getName());
+
+        if(user == null){
+            User newUser = new User(); 
+            newUser.setAuthId(principal.getName());  
+            userService.save(newUser);   
+            return newUser.getTrades(); 
+        }  
+
+        return userService.getTrades(user); 
+    }   
+}
