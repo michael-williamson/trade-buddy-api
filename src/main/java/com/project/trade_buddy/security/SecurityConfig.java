6baab7443e98f77dc6bbec4e64bd97df3ab3a0a6
michael-williamson <michaeldavid1721@yahoo.com>
2020-12-10 10:26:59 -0600

    initialized jwt handling

---
+++ src/main/java/com/project/trade_buddy/security/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.project.trade_buddy.security;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+
+@EnableWebSecurity
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${auth0.audience}")
+    private String audience;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
+                JwtDecoders.fromOidcIssuerLocation(issuer);
+
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);  
+
+        return jwtDecoder;
+    }  
+    
+    @Override  
+    public void configure(HttpSecurity http) throws Exception {  
+        http.cors().and().authorizeRequests()
+        .antMatchers(HttpMethod.OPTIONS,"*").permitAll()
+        .antMatchers(HttpMethod.POST,"*").permitAll()
+        .antMatchers(HttpMethod.GET,"*").permitAll()
+        .antMatchers(HttpMethod.DELETE,"*").permitAll()
+        .antMatchers(HttpMethod.PUT,"*").permitAll()
+                .mvcMatchers("/api/authorize/trades").authenticated()
+                .mvcMatchers("/api/authorize/user").authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+                
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("108.69.29.46","108.69.29.46"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE","PUT","OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization","content-type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
+ 
