bbacd51e03e2fdf059ace28ba693567c74518927
michael-williamson <michaeldavid1721@yahoo.com>
2020-12-16 16:42:38 -0600

    help connecting to db and recieving requests

--- src/main/java/com/project/trade_buddy/controller/TradesController.java
+++ src/main/java/com/project/trade_buddy/controller/TradesController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "https://main--trade-buddy.netlify.app/", maxAge = 3600)
+@CrossOrigin(origins = "https://main--trade-buddy.netlify.app", maxAge = 3600)
 @RestController
 @RequestMapping(path = "/api/authorize", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TradesController {
